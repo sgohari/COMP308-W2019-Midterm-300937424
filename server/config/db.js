1f4cdd52afc1898b978347e2ce7b1f04115a0a85
sgohary <syednacer@gmail.com>
2019-02-23 16:29:39 -0500

    CRUD operation is implemented

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books308"
+  //Remote MongoDB deployment -> let 
+  "URI" :"mongodb://thomas:123456@ds161059.mlab.com:61059/books308"
+  //"URI" : "mongodb://nacer:amin2005@ds018308.mlab.com:18308/books308"
+};
